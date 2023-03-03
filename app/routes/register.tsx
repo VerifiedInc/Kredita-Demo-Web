c923e1899ee1af21be05fcc13e2280c860fdecb8
allisonrod <117668984+allisonrod@users.noreply.github.com>
2023-03-03 14:15:23 -0600

    feat: implement user registration and credentials flow (#7)
    * feat: added credential request flow
    
    * style(register): formatting
    
    * refactor: updated comment and return type for sharedCredentials function in coreAPI.server

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,4 +1,9 @@
-import { ActionFunction, json } from '@remix-run/node';
+import {
+  ActionFunction,
+  json,
+  LoaderFunction,
+  redirect,
+} from '@remix-run/node';
 import { Form, useActionData } from '@remix-run/react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -8,6 +13,9 @@ import Typography from '@mui/material/Typography';
 import { red } from '~/styles/colors';
 import { createUserSession } from '~/session.server';
 import { getErrorMessage, getErrorStatus } from '~/errors';
+import { hasMatchingCredentials, sharedCredentials } from '~/coreAPI.server';
+import { config } from '~/config';
+import { logger } from '~/logger.server';
 
 interface ActionData {
   error?: string;
@@ -18,30 +26,65 @@ export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
 
   const email = formData.get('email');
-  const password = formData.get('password');
   const phone = formData.get('phone');
 
   if (!email) {
     return json({ error: 'Email is required' }, { status: 400 });
   }
 
-  if (!password) {
-    return json({ error: 'Password is required' }, { status: 400 });
+  if (!phone) {
+    return json({ error: 'Phone is required' }, { status: 400 });
   }
 
-  if (
-    typeof email !== 'string' ||
-    typeof password !== 'string' ||
-    (phone && typeof phone !== 'string')
-  ) {
+  if (typeof email !== 'string' || typeof phone !== 'string') {
     return json({ error: 'Invalid form data' }, { status: 400 });
   }
 
   try {
-    return createUserSession(request, email);
+    // Check whether the user has existing credentials
+    const credentialRequestUrl = await hasMatchingCredentials(email, phone);
+
+    if (credentialRequestUrl) {
+      const url = new URL(
+        String(credentialRequestUrl).toLowerCase().includes('wallet')
+          ? credentialRequestUrl
+          : config.unumWalletUrl + credentialRequestUrl
+      );
+      logger.info(`URL: ${url}`);
+      // user's email address
+      url.searchParams.set('email', email);
+      // user's phone number
+      url.searchParams.set('phone', phone);
+      // url to redirect the user to once the Unum ID credential request flow is complete
+      url.searchParams.set('redirectUrl', config.demoUrl + '/register');
+
+      // redirect the user to the url returned from the POST request to hasMatchingCredentials
+      return redirect(String(url));
+    }
+    return null;
   } catch (e) {
     return json({ error: getErrorMessage(e) }, { status: getErrorStatus(e) });
   }
+};
+
+// The exported `loader` function will be called when the route makes a GET request, i.e. when it is rendered
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const { searchParams } = url;
+
+  const sharedCredentialsUuid = searchParams.get('sharedCredentialsUuid');
+
+  // If a sharedCredentialsUuid parameter exists, retrieve the associated credentials and
+  // create the user's session - re-directing them to /verified
+  if (sharedCredentialsUuid) {
+    const result = await sharedCredentials(sharedCredentialsUuid);
+    if (result) {
+      const { email } = result;
+      return createUserSession(request, String(email), '/verified');
+    }
+  }
+
+  return null;
 };
 
 export default function Register() {
@@ -58,12 +101,6 @@ export default function Register() {
           alignItems='center'
         >
           <TextField label='Email' name='email' />
-          <TextField
-            label='Password'
-            name='password'
-            type='password'
-            sx={{ marginTop: 2 }}
-          />
           <TextField label='Phone' name='phone' sx={{ marginTop: 2 }} />
           <Button variant='contained' type='submit' sx={{ marginTop: 4 }}>
             Register
